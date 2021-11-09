1fceefe31f762fe6f4c197ba0ac0128947f1357e
Szeles Andor <szelesandor@protonmail.com>
2021-11-09 11:08:27 +0100

    Solve problem

---
+++ Problems/Algorithm/Algorithm I/09_reverse_words_in_a_string_iii.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join([word[::-1] for word in s.split()])
