97516b61d9e178850ddf51a6c5943c0cdd1b610b
Szeles Andor <szelesandor@protonmail.com>
2021-11-10 13:39:08 +0100

    Optimize solution

--- Problems/Dynamic Programming/Dynamic Programming I/jump_game_ii.py
+++ Problems/Dynamic Programming/Dynamic Programming I/jump_game_ii.py
@@ -2,10 +2,12 @@
 
 class Solution:
     def jump(self, nums: list[int]) -> int:
-        distances = [float('inf') for _ in range(len(nums))]
-        distances[-1] = 0
-        for i in reversed(range(len(nums) - 1)):
-            max_jump = nums[i]
-            if max_jump != 0:
-                distances[i] = 1 + min(distances[i + 1: i + max_jump + 1])
-        return int(distances[0])
+        curr_pos = 0
+        best_jump_pos = 0
+        jumps = 0
+        for i in range(len(nums) - 1):
+            best_jump_pos = max(best_jump_pos, i + nums[i])
+            if i == curr_pos:
+                curr_pos = best_jump_pos
+                jumps += 1
+        return jumps
