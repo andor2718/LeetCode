6746184fcd84bc943a434d33f5378c5963d09347
Szeles Andor <szelesandor@protonmail.com>
2021-11-10 12:03:24 +0100

    Add alternative solution

--- Problems/Data Structure/Data Structure I/08_pascals_triangle.py
+++ Problems/Data Structure/Data Structure I/08_pascals_triangle.py
@@ -1,11 +1,8 @@
 # https://leetcode.com/problems/pascals-triangle/
 
+import math
+
+
 class Solution:
-    def generate(self, numRows: int) -> list[list[int]]:
-        result = [[1]]
-        for n in range(1, numRows):
-            row = [1 for _ in range(n + 1)]
-            for i in range(1, n):
-                row[i] = result[-1][i - 1] + result[-1][i]
-            result.append(row)
-        return result
+    def generate(self, nrRows: int) -> list[list[int]]:
+        return [[math.comb(n, k) for k in range(n + 1)] for n in range(nrRows)]
