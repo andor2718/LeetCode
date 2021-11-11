115581ec7bc921f9cbc27279973d0a36cd937198
Szeles Andor <szelesandor@protonmail.com>
2021-11-11 23:40:43 +0100

    Refactor solution

--- Problems/Dynamic Programming/Dynamic Programming I/11_maximum_sum_circular_subarray.py
+++ Problems/Dynamic Programming/Dynamic Programming I/11_maximum_sum_circular_subarray.py
@@ -1,14 +1,19 @@
 # https://leetcode.com/problems/maximum-sum-circular-subarray/
 
-def greatest_subarray_sum(nums: list[int], negative: bool = False) -> int:
-    def func(x: list[int]) -> int:
-        return min(x) if negative else max(x)
+def greatest_subarray_sum(nums: list[int], inverse_mode: bool = False) -> int:
+    """
+    Find the greatest subarray sum in a list of integer numbers,
+    or find the smallest subarray sum using the inverse_mode.
+    """
+
+    def min_max_helper(x: list[int]) -> int:
+        return min(x) if inverse_mode else max(x)
 
     result = nums[0]
     last_total = nums[0]
     for num in nums[1:]:
-        curr_total = func([last_total + num, num])
-        result = func([result, curr_total])
+        curr_total = min_max_helper([last_total + num, num])
+        result = min_max_helper([result, curr_total])
         last_total = curr_total
     return result
 
@@ -18,6 +23,8 @@ class Solution:
         max_without_wrap = greatest_subarray_sum(nums)
         if max(nums) <= 0:
             return max_without_wrap
-        min_without_wrap = greatest_subarray_sum(nums, negative=True)
-        max_with_wrap = sum(nums) - min_without_wrap
-        return max(max_without_wrap, max_with_wrap)
+        min_without_wrap = greatest_subarray_sum(nums, inverse_mode=True)
+        if min_without_wrap >= 0:
+            return max_without_wrap
+        max_with_possible_wrap = sum(nums) - min_without_wrap
+        return max(max_without_wrap, max_with_possible_wrap)
