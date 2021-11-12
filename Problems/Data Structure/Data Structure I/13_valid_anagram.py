5ef2a33485c0f8900a2cb00dcaf9bbe8fc848cd8
Szeles Andor <szelesandor@protonmail.com>
2021-11-12 20:42:18 +0100

    Add alternative solution

--- Problems/Data Structure/Data Structure I/13_valid_anagram.py
+++ Problems/Data Structure/Data Structure I/13_valid_anagram.py
@@ -1,19 +1,12 @@
 # https://leetcode.com/problems/valid-anagram/
 
-def canConstruct(ransomNote: str, magazine: str) -> bool:
-    available_chars = dict()
-    for char in magazine:
-        available_chars[char] = available_chars.get(char, 0) + 1
-    for char in ransomNote:
-        if char not in available_chars or available_chars[char] == 0:
-            return False
-        else:
-            available_chars[char] -= 1
-    return True
-
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        return canConstruct(s, t)
+        s_char_freq = dict()
+        t_char_freq = dict()
+        for s_char, t_char in zip(s, t):
+            s_char_freq[s_char] = s_char_freq.get(s_char, 0) + 1
+            t_char_freq[t_char] = t_char_freq.get(t_char, 0) + 1
+        return s_char_freq == t_char_freq
