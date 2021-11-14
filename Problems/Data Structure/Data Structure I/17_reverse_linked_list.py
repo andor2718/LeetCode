1bafd05128c49362937a19319c1421964afbedca
Szeles Andor <szelesandor@protonmail.com>
2021-11-14 19:10:20 +0100

    Optimize solution

--- Problems/Data Structure/Data Structure I/17_reverse_linked_list.py
+++ Problems/Data Structure/Data Structure I/17_reverse_linked_list.py
@@ -12,15 +12,11 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        values = []
-        visitor = head
-        while visitor:
-            values.append(visitor.val)
-            visitor = visitor.next
-        idx = len(values) - 1
-        visitor = head
-        while visitor:
-            visitor.val = values[idx]
-            idx -= 1
-            visitor = visitor.next
-        return head
+        prev = None
+        curr = head
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
