f5be1e6573c2beecd57eb565cb762fbe12179e9f
Szeles Andor <szelesandor@protonmail.com>
2021-11-15 23:27:24 +0100

    Add a minor optimization

--- Problems/Dynamic Programming/Dynamic Programming I/20_trapping_rain_water.py
+++ Problems/Dynamic Programming/Dynamic Programming I/20_trapping_rain_water.py
@@ -7,13 +7,13 @@ class Solution:
         water = 0
         # Scan heights from left to right
         max_left = height[0]
-        for idx in range(1, len(height)):
+        for idx in range(1, len(height) - 1):
             if height[idx] < max_left:
                 held_water[idx] = max_left - height[idx]
             max_left = max(max_left, height[idx])
         # Scan heights from right to left
         max_right = height[-1]
-        for idx in reversed(range(len(height) - 1)):
+        for idx in reversed(range(1, len(height) - 1)):
             if height[idx] < max_right:
                 held_water[idx] = min(held_water[idx], max_right - height[idx])
             else:
