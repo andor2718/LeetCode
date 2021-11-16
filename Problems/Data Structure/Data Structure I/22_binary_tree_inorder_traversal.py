7536f4d62621d87d254feb46e1ff679541403dce
Szeles Andor <szelesandor@protonmail.com>
2021-11-16 18:16:51 +0100

    Solve problem iteratively

--- Problems/Data Structure/Data Structure I/22_binary_tree_inorder_traversal.py
+++ Problems/Data Structure/Data Structure I/22_binary_tree_inorder_traversal.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/binary-tree-inorder-traversal/
 
 from typing import Optional
+from enum import Enum, auto
 
 
 # Definition for a binary tree node.
@@ -11,10 +12,27 @@ class TreeNode:
         self.right = right
 
 
+class Operations(Enum):
+    PROCESS_NODE = auto()
+    STORE_VALUE = auto()
+
+
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> list[int]:
         if not root:
             return []
-        left = self.inorderTraversal(root.left)
-        right = self.inorderTraversal(root.right)
-        return left + [root.val] + right
+        result = list()
+        stack = [(Operations.PROCESS_NODE, root.right),
+                 (Operations.STORE_VALUE, root),
+                 (Operations.PROCESS_NODE, root.left)]
+        while stack:
+            op, node = stack.pop()
+            if not node:
+                continue
+            if op is Operations.STORE_VALUE:
+                result.append(node.val)
+            else:
+                stack.append((Operations.PROCESS_NODE, node.right))
+                stack.append((Operations.STORE_VALUE, node))
+                stack.append((Operations.PROCESS_NODE, node.left))
+        return result
