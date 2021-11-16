f81516509d0ec60d61773d6d2d2d5be274f65aac
Szeles Andor <szelesandor@protonmail.com>
2021-11-16 18:14:51 +0100

    Solve problem

---
+++ Problems/Data Structure/Data Structure I/21_binary_tree_preorder_traversal.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> list[int]:
+        if not root:
+            return []
+        left = self.preorderTraversal(root.left)
+        right = self.preorderTraversal(root.right)
+        return [root.val] + left + right
