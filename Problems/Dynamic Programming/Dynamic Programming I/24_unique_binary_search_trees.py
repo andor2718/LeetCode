95729bf0231d3fb34ca1601f468bfe7977ac5e69
Szeles Andor <szelesandor@protonmail.com>
2021-11-17 19:30:52 +0100

    Add alternative solution

--- Problems/Dynamic Programming/Dynamic Programming I/24_unique_binary_search_trees.py
+++ Problems/Dynamic Programming/Dynamic Programming I/24_unique_binary_search_trees.py
@@ -1,19 +1,17 @@
 # https://leetcode.com/problems/unique-binary-search-trees/
 
-def _num_trees(n: int, memo: dict[int, int]) -> int:
-    if n in memo:
-        return memo[n]
-    n -= 1  # The root node is fixed.
-    result = 0
-    for i in range(n + 1):
-        left_nodes = i
-        right_nodes = n - i
-        result += _num_trees(left_nodes, memo) * _num_trees(right_nodes, memo)
-    memo[n + 1] = result  # Remember to compensate for the root!
-    return result
-
-
 class Solution:
     def numTrees(self, n: int) -> int:
-        memo = {0: 1, 1: 1}
-        return _num_trees(n, memo)
+        if n <= 1:
+            return 1
+        nums = [0 for _ in range(n + 1)]
+        nums[0] = nums[1] = 1
+        for idx in range(2, n + 1):
+            nodes_without_root = idx - 1
+            unique_trees = 0
+            for i in range(nodes_without_root + 1):
+                left_nodes = i
+                right_nodes = nodes_without_root - left_nodes
+                unique_trees += nums[left_nodes] * nums[right_nodes]
+            nums[idx] = unique_trees
+        return nums[-1]
