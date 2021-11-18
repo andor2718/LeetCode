651c9856ae9dbd86fdcd2dd7dc9f3b80e749b349
Szeles Andor <szelesandor@protonmail.com>
2021-11-18 12:23:47 +0100

    Add alternative solution

--- Problems/Algorithm/Algorithm I/29_number_of_1_bits.py
+++ Problems/Algorithm/Algorithm I/29_number_of_1_bits.py
@@ -2,4 +2,8 @@
 
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        return f'{n:b}'.count('1')
+        cnt = 0
+        while n:
+            cnt += n & 1
+            n >>= 1
+        return cnt
