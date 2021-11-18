0aec9f8e36e3dd38771af0a73dc042f3563aabe7
Szeles Andor <szelesandor@protonmail.com>
2021-11-18 14:10:42 +0100

    Add alternative solution

--- Problems/Dynamic Programming/Dynamic Programming I/26_pascals_triangle_ii.py
+++ Problems/Dynamic Programming/Dynamic Programming I/26_pascals_triangle_ii.py
@@ -1,27 +1,11 @@
 # https://leetcode.com/problems/pascals-triangle-ii/
 
-def fact(n: int) -> int:
-    if n < 0:
-        raise ValueError('fact() not defined for negative values')
-    if n <= 1:
-        return 1
-    result = 1
-    for factor in range(2, n + 1):
-        result *= factor
-    return result
-
-
-def choose(n: int, k: int) -> int:
-    if 0 <= k < n:
-        return fact(n) // (fact(k) * fact(n - k))
-    else:
-        return 0
-
-
 class Solution:
     def getRow(self, rowIndex: int) -> list[int]:
-        row = [1 for _ in range(rowIndex + 1)]
-        if rowIndex >= 2:
-            for k in range(1, rowIndex):
-                row[k] = choose(rowIndex, k)
-        return row
+        last_row = [1]
+        for row_nr in range(1, rowIndex + 1):
+            curr_row = [1 for _ in range(row_nr + 1)]
+            for idx in range(1, row_nr):
+                curr_row[idx] = last_row[idx - 1] + last_row[idx]
+            last_row = curr_row
+        return last_row
