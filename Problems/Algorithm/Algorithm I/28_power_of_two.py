8790ce94e0c317a7a2a004972ffb67d9dcb74a55
Szeles Andor <szelesandor@protonmail.com>
2021-11-18 12:08:31 +0100

    Add alternative solution

--- Problems/Algorithm/Algorithm I/28_power_of_two.py
+++ Problems/Algorithm/Algorithm I/28_power_of_two.py
@@ -4,4 +4,4 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n <= 1:
             return n == 1
-        return sum(map(int, f'{n:b}'[1:])) == 0
+        return int(f'{n:b}'[1:]) == 0
