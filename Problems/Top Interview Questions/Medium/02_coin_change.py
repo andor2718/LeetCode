e2261f21c8df92472fdff6b1319450b7767a29db
Szeles Andor <szelesandor@protonmail.com>
2021-11-19 19:30:04 +0100

    Optimize solution

--- Problems/Top Interview Questions/Medium/02_coin_change.py
+++ Problems/Top Interview Questions/Medium/02_coin_change.py
@@ -7,13 +7,9 @@ class Solution:
         inf = float('inf')
         min_amounts = [inf for _ in range(amount + 1)]
         min_amounts[0] = 0
-        for amount_idx in range(1, len(min_amounts)):
-            curr_amount = amount_idx
-            for coin_idx in range(len(coins)):
-                coin = coins[coin_idx]
-                if coin > curr_amount:
-                    continue
-                curr = min_amounts[curr_amount - coin] + 1
-                min_amounts[amount_idx] = min(min_amounts[amount_idx], curr)
+        for coin in coins:
+            for amount in range(coin, len(min_amounts)):
+                min_amounts[amount] = min(
+                    min_amounts[amount], min_amounts[amount - coin] + 1)
         result = min_amounts[-1]
         return result if result != inf else -1
