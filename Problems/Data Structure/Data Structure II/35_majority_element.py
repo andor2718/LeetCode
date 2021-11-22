28ba6ddd0cad1a94394c5f91b7cf33a3073217ca
Szeles Andor <szelesandor@protonmail.com>
2021-11-22 22:44:39 +0100

    Add alternative solution

--- Problems/Data Structure/Data Structure II/35_majority_element.py
+++ Problems/Data Structure/Data Structure II/35_majority_element.py
@@ -1,10 +1,31 @@
 # https://leetcode.com/problems/majority-element/
 
+def get_end_idx(nums: list[int], low: int, high: int, target: int) -> int:
+    while low <= high:
+        middle = (low + high) // 2
+        guess = nums[middle]
+        if guess == target:
+            if middle == len(nums) - 1 or nums[middle + 1] > target:
+                return middle
+            else:
+                low = middle + 1
+        elif guess < target:
+            low = middle + 1
+        else:
+            high = middle - 1
+    return -1
+
+
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
         majority_threshold = len(nums) // 2
-        num_counts = dict()
-        for num in nums:
-            num_counts[num] = num_counts.get(num, 0) + 1
-            if num_counts[num] > majority_threshold:
+        nums.sort()
+        start = 0
+        while start < len(nums):
+            num = nums[start]
+            end = get_end_idx(nums, start, len(nums) - 1, num)
+            cnt = end - start + 1
+            if cnt > majority_threshold:
                 return num
+            else:
+                start = end + 1
