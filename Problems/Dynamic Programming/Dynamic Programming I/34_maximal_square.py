92917ce9c7fcb06244e3790a9df7d15cd7ff5cad
Szeles Andor <szelesandor@protonmail.com>
2021-11-25 17:42:42 +0100

    Solve problem

---
+++ Problems/Dynamic Programming/Dynamic Programming I/34_maximal_square.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/maximal-square/
+
+class Solution:
+    def maximalSquare(self, matrix: list[list[str]]) -> int:
+        max_square = 0
+        matrix = [list(map(int, row)) for row in matrix]  # Convert str to int
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for row in range(rows):
+            for col in range(cols):
+                if matrix[row][col] == 1:
+                    top = matrix[row - 1][col] if row > 0 else 0
+                    left = matrix[row][col - 1] if col > 0 else 0
+                    diagonal_top_left = (
+                        matrix[row - 1][col - 1] if row > 0 and col > 0 else 0)
+                    matrix[row][col] += min(top, left, diagonal_top_left)
+                    max_square = max(max_square, matrix[row][col])
+        return max_square * max_square
