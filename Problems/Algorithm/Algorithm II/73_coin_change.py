240fb3cc3f08aad0cb44ae8efdeff6e900324a8e
Szeles Andor <szelesandor@protonmail.com>
2021-12-07 11:03:45 +0100

    Reuse previous solution from Dynamic Programming I

---
+++ Problems/Algorithm/Algorithm II/73_coin_change.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/coin-change/
+
+class Solution:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        inf = float('inf')
+        min_amounts = [inf for _ in range(amount + 1)]
+        min_amounts[0] = 0
+        for coin in coins:
+            for amount in range(coin, len(min_amounts)):
+                min_amounts[amount] = min(
+                    min_amounts[amount], min_amounts[amount - coin] + 1)
+        result = min_amounts[-1]
+        return result if result != inf else -1
