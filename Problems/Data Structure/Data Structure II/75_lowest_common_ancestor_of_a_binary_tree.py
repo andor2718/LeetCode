ae152289138b33014751d68678fd1d711f255392
Szeles Andor <szelesandor@protonmail.com>
2021-12-09 13:30:00 +0100

    Optimize solution

--- Problems/Data Structure/Data Structure II/75_lowest_common_ancestor_of_a_binary_tree.py
+++ Problems/Data Structure/Data Structure II/75_lowest_common_ancestor_of_a_binary_tree.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 
+from typing import Optional
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
@@ -8,28 +11,21 @@ class TreeNode:
         self.right = None
 
 
-def get_path(root: TreeNode, target: TreeNode, path: list[TreeNode]) -> bool:
-    path.append(root)
-    if root == target:
-        return True
-    if root.left:
-        if get_path(root.left, target, path):
-            return True
-    if root.right:
-        if get_path(root.right, target, path):
-            return True
-    path.pop()
-    return False
-
-
 class Solution:
     def lowestCommonAncestor(
-            self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        path_to_p = list()
-        get_path(root, p, path_to_p)
-        path_to_p = set(path_to_p)
-        path_to_q = list()
-        get_path(root, q, path_to_q)
-        for node in reversed(path_to_q):
-            if node in path_to_p:
-                return node
+            self, root: Optional[TreeNode], p: TreeNode, q: TreeNode
+    ) -> Optional[TreeNode]:
+        if not root:
+            return None
+        if root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        elif left:
+            return left
+        elif right:
+            return right
+        else:
+            return None
