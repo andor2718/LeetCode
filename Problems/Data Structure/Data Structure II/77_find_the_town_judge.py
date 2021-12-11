78646407263f60fc8adaa8f5271dfc22553e4f47
Szeles Andor <szelesandor@protonmail.com>
2021-12-11 12:35:24 +0100

    Optimize solution

--- Problems/Data Structure/Data Structure II/77_find_the_town_judge.py
+++ Problems/Data Structure/Data Structure II/77_find_the_town_judge.py
@@ -2,20 +2,20 @@
 
 class Solution:
     def findJudge(self, n: int, trust: list[list[int]]) -> int:
-        trusted_by = [[] for _ in range(n + 1)]  # We'll ignore the 0th element
-        for truster, trusted in trust:
-            trusted_by[trusted].append(truster)
+        trust_counts = [0 for _ in range(n + 1)]  # We'll ignore the 0th entry.
+        for _, trusted in trust:
+            trust_counts[trusted] += 1
         judge_candidate = None
         # Find the only candidate, if exists
-        for trusted, trusting_people in enumerate(trusted_by[1:], start=1):
-            if len(trusting_people) == n - 1:
+        for trusted, trust_count in enumerate(trust_counts[1:], start=1):
+            if trust_count == n - 1:
                 if judge_candidate is None:
                     judge_candidate = trusted
                 else:
                     return -1
-        if judge_candidate is None:  # No candidate found
+        if judge_candidate is None:  # No candidate found.
             return -1
-        else:  # Check if candidate trusts anybody
+        else:  # Check if candidate trusts anybody.
             for truster, _ in trust:
                 if truster == judge_candidate:
                     return -1
