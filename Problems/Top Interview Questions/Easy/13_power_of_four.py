285743050ab63e99eb774cb53bda4eeabf78fd7c
Szeles Andor <szelesandor@protonmail.com>
2022-01-02 23:49:00 +0100

    Solve problem

---
+++ Problems/Top Interview Questions/Easy/13_power_of_four.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/power-of-four/
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        """
+        Returns True if n is positive, a power of 2, and the set bit is
+        at an odd index (one-based) from right to left.
+        Returns False otherwise.
+        """
+        if n <= 0:
+            return False
+        if (n & (n - 1)) != 0:
+            return False
+        bitmask = int(b'01' * 16, 2)  # We need no more than 32 bits to check.
+        return n == n & bitmask
