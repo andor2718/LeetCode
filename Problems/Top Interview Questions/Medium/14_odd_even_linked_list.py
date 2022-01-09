d5879aa1d283712dc6d942246a069a82481242f4
Szeles Andor <szelesandor@protonmail.com>
2022-01-09 23:13:13 +0100

    Optimize solution

--- Problems/Top Interview Questions/Medium/14_odd_even_linked_list.py
+++ Problems/Top Interview Questions/Medium/14_odd_even_linked_list.py
@@ -17,16 +17,16 @@ class Solution:
         even_head = head.next
         last_odd, last_even = head, even_head
         curr = even_head.next
-        curr_nr = 3
+        curr_is_even = False
         while curr:
-            if curr_nr % 2 == 0:  # Current node is even.
+            if curr_is_even:
                 last_even.next = curr
                 last_even = curr
-            else:  # Current node is odd.
+            else:
                 last_odd.next = curr
                 last_odd = curr
             curr = curr.next
-            curr_nr += 1
+            curr_is_even = not curr_is_even
         last_even.next = None
         last_odd.next = even_head
         return head
