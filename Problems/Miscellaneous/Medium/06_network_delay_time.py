be90f593e922535cb35dac484d94e32e2a3fd8f2
Szeles Andor <szelesandor@protonmail.com>
2022-01-15 23:56:53 +0100

    Refactor solution

--- Problems/Miscellaneous/Medium/06_network_delay_time.py
+++ Problems/Miscellaneous/Medium/06_network_delay_time.py
@@ -1,35 +1,33 @@
 # https://leetcode.com/problems/network-delay-time/
 
-from typing import Union
-
-
 class Graph:
-    def __init__(self, node_cnt: int, weighted_edges: list[list[int]]):
-        self._neighbors_and_weights = {
+    def __init__(self, node_cnt: int, edges_with_dists: list[list[int]]):
+        self._neighbors_and_dists = {
             node: list() for node in range(1, node_cnt + 1)}
-        for source, target, weight in weighted_edges:
-            self._neighbors_and_weights[source].append((target, weight))
+        for source, target, dist in edges_with_dists:
+            self._neighbors_and_dists[source].append((target, dist))
 
-    def get_max_dist(self, source: int) -> Union[int, float]:
+    def get_max_dist(self, source: int) -> int:
         # Let's utilize Dijkstra's algorithm!
         inf = float('inf')
-        distances = {node: inf for node in self._neighbors_and_weights.keys()}
-        distances[source] = 0
-        nodes_to_process = set(self._neighbors_and_weights.keys())
+        dists_from_source = {
+            node: inf for node in self._neighbors_and_dists.keys()}
+        dists_from_source[source] = 0
+        nodes_to_process = set(self._neighbors_and_dists.keys())
         while nodes_to_process:
-            curr_node = min(nodes_to_process, key=lambda x: distances[x])
+            curr_node = min(nodes_to_process, key=dists_from_source.get)
             nodes_to_process.remove(curr_node)
-            curr_dist = distances[curr_node]
-            if curr_dist == inf:  # Graph is not connected
-                return inf
-            for node, weight in self._neighbors_and_weights[curr_node]:
+            curr_dist_from_source = dists_from_source[curr_node]
+            if curr_dist_from_source == inf:  # Graph is not connected
+                return -1
+            for node, dist in self._neighbors_and_dists[curr_node]:
                 if node in nodes_to_process:
-                    distances[node] = min(distances[node], curr_dist + weight)
-        return max(distances.values())
+                    dists_from_source[node] = min(
+                        dists_from_source[node], curr_dist_from_source + dist)
+        return max(dists_from_source.values())
 
 
 class Solution:
     def networkDelayTime(self, times: list[list[int]], n: int, k: int) -> int:
         graph = Graph(n, times)
-        max_dist = graph.get_max_dist(k)
-        return max_dist if max_dist != float('inf') else -1
+        return graph.get_max_dist(k)
