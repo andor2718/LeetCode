5d95d014b67a55de343e6ab8b5fd6f2845c0f55c
Szeles Andor <szelesandor@protonmail.com>
2022-02-10 20:38:32 +0100

    Optimize solution to use only O(1) extra space

--- Problems/Miscellaneous/Easy/13_sort_array_by_parity.py
+++ Problems/Miscellaneous/Easy/13_sort_array_by_parity.py
@@ -2,10 +2,19 @@
 
 class Solution:
     def sortArrayByParity(self, nums: list[int]) -> list[int]:
-        evens, odds = list(), list()
-        for num in nums:
-            if num & 1 == 0:
-                evens.append(num)
+        left, right = 0, len(nums) - 1
+        while left < right:
+            left_is_even = nums[left] & 1 == 0
+            right_is_even = nums[right] & 1 == 0
+            if left_is_even:
+                if right_is_even:
+                    left += 1
+                else:
+                    left, right = left + 1, right - 1
             else:
-                odds.append(num)
-        return evens + odds
+                if right_is_even:
+                    nums[left], nums[right] = nums[right], nums[left]
+                    left, right = left + 1, right - 1
+                else:
+                    right -= 1
+        return nums
