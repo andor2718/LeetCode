8ec9998a56b338cdfc8525adc3a83de0a69c6173
Szeles Andor <szelesandor@protonmail.com>
2022-02-11 19:37:38 +0100

    Refactor solution

--- Problems/Study Plans/Algorithm/Algorithm II/33_search_in_rotated_sorted_array.py
+++ Problems/Study Plans/Algorithm/Algorithm II/33_search_in_rotated_sorted_array.py
@@ -2,28 +2,19 @@
 
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        low = 0
-        high = len(nums) - 1
+        low, high = 0, len(nums) - 1
         while low <= high:
-            middle = (high + low) // 2
-            guess = nums[middle]
-            if guess == target:
-                return middle
-            else:
-                if nums[low] <= guess:  # Sorted form nums[low] to nums[middle]
-                    if guess < target:
-                        low = middle + 1
-                    else:
-                        if nums[low] <= target:
-                            high = middle - 1
-                        else:
-                            low = middle + 1
-                else:  # Sorted form nums[middle] to nums[high]
-                    if guess < target:
-                        if target <= nums[high]:
-                            low = middle + 1
-                        else:
-                            high = middle - 1
-                    else:
-                        high = middle - 1
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[low] <= nums[mid]:  # Sorted from nums[low] to nums[mid]
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:  # Sorted from nums[mid] to nums[high]
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
         return -1
