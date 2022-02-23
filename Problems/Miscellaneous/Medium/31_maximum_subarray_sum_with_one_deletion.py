f1468f469034fb3efccd5d6fd25bb45fc4dd7235
Szeles Andor <szelesandor@protonmail.com>
2022-02-23 17:47:27 +0100

    Optimize solution

--- Problems/Miscellaneous/Medium/31_maximum_subarray_sum_with_one_deletion.py
+++ Problems/Miscellaneous/Medium/31_maximum_subarray_sum_with_one_deletion.py
@@ -1,32 +1,17 @@
 # https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/
 
-def get_max_subarray_sums(arr: list[int], reverse: bool = False) -> list[int]:
-    max_subarray_sums = [arr[0]] if not reverse else [arr[-1]]
-    last_max = max_subarray_sums[0]
-    for num in arr[1:] if not reverse else reversed(arr[:-1]):
-        curr_max = max(num, last_max + num)
-        max_subarray_sums.append(curr_max)
-        last_max = curr_max
-    if reverse:
-        max_subarray_sums.reverse()
-    return max_subarray_sums
+from typing import Union
 
 
 class Solution:
     def maximumSum(self, arr: list[int]) -> int:
-        if len(arr) == 1:
-            return arr[0]
-        max_subarray_sum = max(arr)
-        if max_subarray_sum <= 0:
-            return max_subarray_sum
-        left_max_sums = get_max_subarray_sums(arr)
-        max_subarray_sum = max(max_subarray_sum, max(left_max_sums))
-        right_max_sums = get_max_subarray_sums(arr, reverse=True)
-        max_subarray_sum = max(max_subarray_sum, max(right_max_sums))
-        # NOTE: len(arr) == len(left_max_sums) == len(right_max_sums).
-        for idx in range(len(arr)):
-            left_sum = left_max_sums[idx - 1] if idx > 0 else None
-            right_sum = right_max_sums[idx + 1] if idx + 1 < len(arr) else None
-            if left_sum is not None and right_sum is not None:
-                max_subarray_sum = max(max_subarray_sum, left_sum + right_sum)
-        return max_subarray_sum
+        last_without_delete: Union[float, int] = float('-inf')
+        last_with_delete: Union[float, int] = float('-inf')
+        max_sum: Union[float, int] = float('-inf')
+        for num in arr:
+            curr_without_delete = max(num, num + last_without_delete)
+            curr_with_delete = max(last_without_delete, num + last_with_delete)
+            max_sum = max(max_sum, curr_with_delete, curr_without_delete)
+            last_with_delete = curr_with_delete
+            last_without_delete = curr_without_delete
+        return max_sum
