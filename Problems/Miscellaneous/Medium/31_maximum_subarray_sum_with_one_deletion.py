47770a106f914c309df66d63b6bfafbb8c41e488
Szeles Andor <szelesandor@protonmail.com>
2022-02-23 17:55:37 +0100

    Refactor solution

--- Problems/Miscellaneous/Medium/31_maximum_subarray_sum_with_one_deletion.py
+++ Problems/Miscellaneous/Medium/31_maximum_subarray_sum_with_one_deletion.py
@@ -7,6 +7,6 @@ class Solution:
             curr_without_delete = max(num, num + last_without_delete)
             curr_with_delete = max(last_without_delete, num + last_with_delete)
             max_sum = max(max_sum, curr_with_delete, curr_without_delete)
-            last_with_delete = curr_with_delete
             last_without_delete = curr_without_delete
+            last_with_delete = curr_with_delete
         return max_sum
