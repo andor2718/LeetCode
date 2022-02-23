8b4e883a2dbc11398ce2be652184b38f2b6da4da
Szeles Andor <szelesandor@protonmail.com>
2022-02-23 17:49:57 +0100

    Remove redundant type hints

--- Problems/Miscellaneous/Medium/31_maximum_subarray_sum_with_one_deletion.py
+++ Problems/Miscellaneous/Medium/31_maximum_subarray_sum_with_one_deletion.py
@@ -1,13 +1,8 @@
 # https://leetcode.com/problems/maximum-subarray-sum-with-one-deletion/
 
-from typing import Union
-
-
 class Solution:
     def maximumSum(self, arr: list[int]) -> int:
-        last_without_delete: Union[float, int] = float('-inf')
-        last_with_delete: Union[float, int] = float('-inf')
-        max_sum: Union[float, int] = float('-inf')
+        last_without_delete = last_with_delete = max_sum = float('-inf')
         for num in arr:
             curr_without_delete = max(num, num + last_without_delete)
             curr_with_delete = max(last_without_delete, num + last_with_delete)
