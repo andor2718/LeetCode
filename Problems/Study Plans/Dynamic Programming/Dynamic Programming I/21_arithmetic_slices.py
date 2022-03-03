74ad3a775d921f8000e0153fefc0c66df403f902
Szeles Andor <szelesandor@protonmail.com>
2022-03-03 19:50:27 +0100

    Simplify solution

--- Problems/Study Plans/Dynamic Programming/Dynamic Programming I/21_arithmetic_slices.py
+++ Problems/Study Plans/Dynamic Programming/Dynamic Programming I/21_arithmetic_slices.py
@@ -1,27 +1,17 @@
 # https://leetcode.com/problems/arithmetic-slices/
 
-def triangle_num(n: int) -> int:
-    return (n * (n + 1)) // 2
-
-
 class Solution:
     def numberOfArithmeticSlices(self, nums: list[int]) -> int:
-        if len(nums) <= 2:
-            return 0
         arithmetic_slices = 0
+        if len(nums) <= 2:
+            return arithmetic_slices
         last_diff = nums[1] - nums[0]
-        connections = 1  # A slice of 2 numbers has 1 connection: num1 -> num2.
-        last_added = 0
+        last_increment = 0
         for idx in range(2, len(nums)):
             curr_diff = nums[idx] - nums[idx - 1]
-            if last_diff == curr_diff:
-                connections += 1
-                if connections >= 2:
-                    curr_triangle_num = triangle_num(connections - 1)
-                    arithmetic_slices += curr_triangle_num - last_added
-                    last_added = curr_triangle_num
-            else:
-                last_diff = curr_diff
-                connections = 1
-                last_added = 0
+            curr_increment = 0
+            if curr_diff == last_diff:
+                curr_increment = last_increment + 1
+            arithmetic_slices += curr_increment
+            last_diff, last_increment = curr_diff, curr_increment
         return arithmetic_slices
