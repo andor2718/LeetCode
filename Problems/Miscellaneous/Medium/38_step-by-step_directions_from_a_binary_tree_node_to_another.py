f3004c40d859b106f07b012e69dc6b2a7b0aff4e
Szeles Andor <szelesandor@protonmail.com>
2022-03-07 22:58:33 +0100

    Optimize solution

--- Problems/Miscellaneous/Medium/38_step-by-step_directions_from_a_binary_tree_node_to_another.py
+++ Problems/Miscellaneous/Medium/38_step-by-step_directions_from_a_binary_tree_node_to_another.py
@@ -1,9 +1,5 @@
 # https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/
 
-from __future__ import annotations
-from typing import Optional
-
-
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -12,70 +8,56 @@ class TreeNode:
         self.right = right
 
 
-class Walker:
-    def __init__(self, node: TreeNode,
-                 prev: Optional[Walker] = None, direction: str = ''):
-        self.node = node
-        self.prev = prev
-        self.direction = direction
-
-
-def get_parent_lut(root: TreeNode) -> dict[int, TreeNode]:
-    parent_lut = dict()
-    nodes = [root]
-    while nodes:
-        next_nodes = list()
-        for node in nodes:
-            for child in [node.left, node.right]:
-                if child:
-                    parent_lut[child.val] = node
-                    next_nodes.append(child)
-        nodes = next_nodes
-    return parent_lut
-
-
-def get_start_node(node: TreeNode, start_value: int) -> TreeNode:
-    nodes = [node]
-    while True:
-        next_nodes = list()
-        for node in nodes:
-            if node.val == start_value:
-                return node
-            for child in [node.left, node.right]:
-                if child:
-                    next_nodes.append(child)
-        nodes = next_nodes
-
-
-def get_dest_walker(start_node: TreeNode, dest_value: int,
-                    parent_lut: dict[int, TreeNode]) -> Walker:
-    walkers = [Walker(start_node)]
-    seen_vals = set()
-    while True:
-        next_walkers = list()
-        for walker in walkers:
-            if walker.node.val == dest_value:
-                return walker
-            seen_vals.add(walker.node.val)
-            if walker.node.left and walker.node.left.val not in seen_vals:
-                next_walkers.append(Walker(walker.node.left, walker, 'L'))
-            if walker.node.right and walker.node.right.val not in seen_vals:
-                next_walkers.append(Walker(walker.node.right, walker, 'R'))
-            if walker.node.val in parent_lut:
-                parent = parent_lut[walker.node.val]
-                if parent.val not in seen_vals:
-                    next_walkers.append(Walker(parent, walker, 'U'))
-        walkers = next_walkers
-
-
 class Solution:
     def getDirections(
             self, root: TreeNode, startValue: int, destValue: int) -> str:
-        parent_lut = get_parent_lut(root)
-        start_node = get_start_node(root, startValue)
-        walker = get_dest_walker(start_node, destValue, parent_lut)
-        directions = list()
-        while walker:
-            directions.append(walker.direction)
-            walker = walker.prev
-        return ''.join(reversed(directions))
+        # Find start and dest nodes and their depths. Make a parent LUT.
+        start_node = dest_node = None
+        curr_depth = start_depth = dest_depth = 0
+        parent_lut = dict()
+        nodes = [root]
+        while nodes:
+            next_nodes = list()
+            for node in nodes:
+                if node.val == startValue:
+                    start_node, start_depth = node, curr_depth
+                elif node.val == destValue:
+                    dest_node, dest_depth = node, curr_depth
+                if start_node is not None and dest_node is not None:
+                    break  # We got all we need.
+                for child in [node.left, node.right]:
+                    if child:
+                        parent_lut[child.val] = node
+                        next_nodes.append(child)
+            nodes = next_nodes
+            curr_depth += 1
+        # Find the lowest common ancestor of start node and dest node.
+        start_runner, dest_runner = start_node, dest_node
+        while start_depth != dest_depth:
+            if start_depth < dest_depth:
+                dest_runner = parent_lut[dest_runner.val]
+                dest_depth -= 1
+            else:
+                start_runner = parent_lut[start_runner.val]
+                start_depth -= 1
+        while start_runner != dest_runner:
+            start_runner = parent_lut[start_runner.val]
+            dest_runner = parent_lut[dest_runner.val]
+        lca_node = start_runner
+        # Collect directions from start to lca.
+        start_to_lca_directions = list()
+        curr_node = start_node
+        while curr_node != lca_node:
+            start_to_lca_directions.append('U')
+            curr_node = parent_lut[curr_node.val]
+        # Collect directions from lca to dest.
+        lca_to_dest_directions = list()
+        curr_node = dest_node
+        while curr_node != lca_node:
+            parent = parent_lut[curr_node.val]
+            direction = 'L' if curr_node == parent.left else 'R'
+            lca_to_dest_directions.append(direction)
+            curr_node = parent
+        lca_to_dest_directions.reverse()
+        # Generate result.
+        return ''.join(start_to_lca_directions + lca_to_dest_directions)
