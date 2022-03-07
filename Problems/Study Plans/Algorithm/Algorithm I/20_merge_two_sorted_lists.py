553a99cea3ca9dba9368e46fe4d22a176e76845c
Szeles Andor <szelesandor@protonmail.com>
2022-03-07 16:43:46 +0100

    Simplify solution

--- Problems/Study Plans/Algorithm/Algorithm I/20_merge_two_sorted_lists.py
+++ Problems/Study Plans/Algorithm/Algorithm I/20_merge_two_sorted_lists.py
@@ -17,20 +17,18 @@ class Solution:
     def mergeTwoLists(
             self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        l1_start = ListNode(next=l1)
-        l2_start = ListNode(next=l2)
-        l1_visitor = l1_start
-        while l1_visitor.next or l2_start.next:
-            if not l1_visitor.next:
-                l1_visitor.next = l2_start.next
-                break
-            if not l2_start.next:
-                break
-            if l2_start.next.val <= l1_visitor.next.val:
-                l2_new_head = l2_start.next.next
-                l1_next_node = l1_visitor.next
-                l1_visitor.next = l2_start.next
-                l1_visitor.next.next = l1_next_node
-                l2_start.next = l2_new_head
-            l1_visitor = l1_visitor.next
-        return l1_start.next
+        sentinel = tail = ListNode()
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                tail = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                tail = l2
+                l2 = l2.next
+        if not l1:
+            tail.next = l2
+        else:
+            tail.next = l1
+        return sentinel.next
