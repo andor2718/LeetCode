e75a94d5760c49d0224cd079e00bc7436fd82c7a
Szeles Andor <szelesandor@protonmail.com>
2022-03-19 22:46:30 +0100

    Optimize solution

--- Problems/Miscellaneous/Hard/09_maximum_frequency_stack.py
+++ Problems/Miscellaneous/Hard/09_maximum_frequency_stack.py
@@ -1,24 +1,20 @@
 # https://leetcode.com/problems/maximum-frequency-stack/
 
-import heapq
-from collections import namedtuple
-
-Entry = namedtuple('Entry', ['freq', 'push_nr', 'num'])
-
-
 class FreqStack:
     def __init__(self):
-        self._push_nr = 0
         self._num_to_freq = dict()
-        self._min_heap = list()
+        self._num_stacks = list()
 
     def push(self, val: int) -> None:
-        push_nr, self._push_nr = self._push_nr, self._push_nr + 1
         self._num_to_freq[val] = self._num_to_freq.get(val, 0) + 1
-        new_entry = Entry(-self._num_to_freq[val], -push_nr, val)
-        heapq.heappush(self._min_heap, new_entry)
+        stack_idx = self._num_to_freq[val] - 1
+        if len(self._num_stacks) == stack_idx:
+            self._num_stacks.append([])
+        self._num_stacks[stack_idx].append(val)
 
     def pop(self) -> int:
-        top_entry = heapq.heappop(self._min_heap)
-        self._num_to_freq[top_entry.num] -= 1
-        return top_entry.num
+        top_num = self._num_stacks[-1].pop()
+        self._num_to_freq[top_num] -= 1
+        if not self._num_stacks[-1]:
+            self._num_stacks.pop()
+        return top_num
