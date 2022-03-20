6445ff98fb76a16bc70dea35abb7a6edeb031136
Szeles Andor <szelesandor@protonmail.com>
2022-03-20 18:40:33 +0100

    Solve problem

---
+++ Problems/Miscellaneous/Medium/48_minimum_domino_rotations_for_equal_row.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
+
+from typing import Union
+
+
+class Solution:
+    def minDominoRotations(self, tops: list[int], bottoms: list[int]) -> int:
+        def _get_min_rotations_for_num(_num: int) -> Union[int, float]:
+            top_rotations = bottom_rotations = 0
+            for idx in range(len(tops)):
+                if tops[idx] != _num and bottoms[idx] != _num:
+                    return float('inf')
+                elif not (tops[idx] == _num and bottoms[idx] == _num):
+                    if tops[idx] == _num:
+                        bottom_rotations += 1
+                    else:
+                        top_rotations += 1
+            return min(top_rotations, bottom_rotations)
+
+        nums = [i for i in range(1, 6 + 1)]
+        min_rotations = float('inf')
+        for num in nums:
+            min_rotations = min(min_rotations, _get_min_rotations_for_num(num))
+        return min_rotations if min_rotations != float('inf') else -1
