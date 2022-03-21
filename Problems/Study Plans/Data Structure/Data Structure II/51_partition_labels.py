d4e6134a5fd06f789ebc27635ab33f660ac1cb23
Szeles Andor <szelesandor@protonmail.com>
2022-03-21 21:40:17 +0100

    Optimize solution

--- Problems/Study Plans/Data Structure/Data Structure II/51_partition_labels.py
+++ Problems/Study Plans/Data Structure/Data Structure II/51_partition_labels.py
@@ -3,22 +3,18 @@
 class Solution:
     def partitionLabels(self, s: str) -> list[int]:
         # Find the start and end index of every char.
-        start_idx_lut = dict()
-        end_idx_lut = dict()
+        start_idx_lut, end_idx_lut = dict(), dict()
         for idx, char in enumerate(s):
             if char not in start_idx_lut:
                 start_idx_lut[char] = idx
             end_idx_lut[char] = idx
         # Build a list of all the intervals in ascending order with
-        # respect to their starting point. (NOTE: start <= end)
-        # But first, we'll sort items by their key, so for every char,
-        # start and end index gets paired properly inside the zip.
+        # respect to their starting point.
+        # (NOTE: start <= end and start_idx_lut is already sorted by values.)
         intervals = []
-        for item1, item2 in zip(sorted(start_idx_lut.items()),
-                                sorted(end_idx_lut.items())):
-            (_, start), (_, end) = item1, item2
+        for char, start in start_idx_lut.items():
+            end = end_idx_lut[char]
             intervals.append((start, end))
-        intervals.sort()  # Sort intervals by their start.
         # Now we'll merge all the overlapping intervals, and
         # we will start a new interval as soon as possible.
         merged_intervals = [intervals[0]]
